63e3d012236d9e1dba62964d051177b718d327cb
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-03-07 12:53:15 +0500

    подготовка

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::string strIP;  
+	std::cout << "Validation IP-address" << std::endl;
+    std::cin >> strIP;
+    std::cout << "ip-address " << strIP << std::endl;
+
+    return 0;
+}
