0c05d5c40363593fc23d23ba3bcd7e828c4ff65b
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-03-14 22:02:02 +0500

    замечания

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 
 bool validator(std::string strIP){
     for(int i = 1; i <= strIP.length(); i++){
-         if((((strIP[0] == '0') && (strIP[0 + i] == '0' && strIP[0 + i + 1] == '0')) 
-           && (strIP[i - 1] != strIP[strIP.length()]))
+         if((((strIP[0] == '0') && (strIP[i] == '0' && strIP[i + 1] == '0')) 
+           && (strIP[i - 1] != strIP[strIP.length() - 1]))
              || (!isdigit(strIP[i - 1]))
                  || ((strIP[i - 1] == '0') && (strIP[i] > '0'))){
             return false;
@@ -15,9 +15,14 @@ bool validator(std::string strIP){
     if(((strIP[0] == '0') && (strIP[1] == '0'))){
         return false;
     }
-    if((strIP[0] >= '2') && (strIP[1] >= '5') && (strIP[2] > '5')){
+    for(int i = 0; i < strIP.length(); i++){
+        if((strIP[i] >= '2') && ((strIP[i+1] >= '5') 
+           && (strIP[i] != strIP[strIP.length() - 1])) 
+             && ((strIP[i+2] > '5') 
+               && (strIP[i] != strIP[strIP.length() - 1]))){
             return false;
-        
+            
+        }
     }
     return true;
 }
@@ -34,7 +39,6 @@ bool genelalPart(std::string strIP, int countPoint){
         strPatr = parsIp(strIP, i);
         if(!validator(strPatr) || strPatr.length() > 3){
             return false;
-            break;
         }
     }
     if(strPatr.empty()){
@@ -50,7 +54,7 @@ int main()
     int countPoint = 0;
 	std::cout << "Validation IP-address" << std::endl;
     std::cin >> strIP_address;
-    for(int i = 0; i < strIP_address.length(); i++){
+    for(int i = 0; i < strIP_address.length() - 1; i++){
         if((strIP_address[i] == '.' && strIP_address[i + 1] == '.') 
           && (strIP_address[i] != strIP_address[strIP_address.length() - 1])){
             flag = false;
